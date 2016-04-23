a5ac95c38173180f340c861c21efd115b0885773
imeoer <imeoer@gmail.com>
2016-04-23 15:25:37 +0800

    add cases section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,12 @@
+var createAssciimaPlayer = function(num) {
+	asciinema_player.core.CreatePlayer('asciicast' + num, '/assets/js/vendor/asciicast/case' + num + '.json', { width: 181, height: 20, speed: 7 });
+}
+
+createAssciimaPlayer(1);
+createAssciimaPlayer(2);
+createAssciimaPlayer(3);
+createAssciimaPlayer(4);
+
 function isSmall()
 {
 	return matchMedia("(max-width:640px)").matches;
@@ -6,13 +15,13 @@ function isSmall()
 
 function isMedium()
 {
-	return matchMedia("(max-width: 1024px)").matches;	
+	return matchMedia("(max-width: 1024px)").matches;
 }
 
 var toggleAccordionNavs = function() {
-	
+
 	var el = $("footer .menu.accordion nav.white");
-	
+
 	if (isSmall())
 		el.hide();
 	else
@@ -20,201 +29,211 @@ var toggleAccordionNavs = function() {
 }
 
 function checkHeaderVideoStatus($start, $video, headerVideoPlaying) {
-	
+
 	if (isSmall()) return;
-	
+
 	// Check the status of the header video
-		
+
 	// If the start section is not in the viewport..
-	
+
 	if ( !$start.is(":in-viewport")) {
-		
+
 		// And the video is playing..
-			
+
 		if (headerVideoPlaying && $video.get(0)) {
-			
+
 			// Pause the video
-			
+
 			$video.get(0).pause();
-		}	
+		}
 	}
-	
+
 	// If is in the viewport
-	
+
 	else {
-		
+
 		// And is not playing
-		
+
 		if (!headerVideoPlaying) {
-			
+
 			// Resume video
-			
+
 			$video.get(0).play();
-			
-			headerVideoPlaying = true;	
-		}	
-	}	
+
+			headerVideoPlaying = true;
+		}
+	}
 }
 
 function checkSlideshowStatus($slideshow, cyclePaused, currentSlideEl) {
-	
+
 	if( $slideshow.is(":in-viewport") ) {
-		
+
 		$slideshow.cycle("resume");
 	}
 	else {
-		
+
 		$slideshow.cycle("pause");
-	}	
+	}
 }
 
 
 
 $(function(){
-	
+
 	var $start = $("section.start");
 	var $video = $("section.start video");
 	var $slideshow = $(".cycle-slideshow");
 	var headerVideoPlaying = true;
 	var cyclePaused;
-	
+
 	checkHeaderVideoStatus($start, $video, headerVideoPlaying);
-	
+
 	$(window).scroll(function(){
 		checkHeaderVideoStatus($start, $video, headerVideoPlaying);
 		checkSlideshowStatus($slideshow, cyclePaused, false);
 	});
 
 	// Flag to check if the header video is playing
-	
+
 	$video.on("pause", function (e) {
-		headerVideoPlaying = false;	
+		headerVideoPlaying = false;
 	});
-	
+
 	$video.on("playing", function (e) {
-		headerVideoPlaying = true;	
+		headerVideoPlaying = true;
 	});
-	
+
 	// Wire up Videos and Slideshow
-	
+
 	$slideshow.on("cycle-paused", function(event, optionHash) {
-		
+
 		if (isSmall()) return;
-		
+
 		$(".cycle-slideshow video").each(function(index, el){
-		    el.pause();    
-	    }); 
-		
+		    el.pause();
+	    });
+
 		cyclePaused = true;
 	});
-	
+
 	$slideshow.on("cycle-resumed", function(event, optionHash) {
-		
+
 		if (isSmall()) return;
-		
+
 		var currentSlide = $(".cycle-slideshow").find(".features__slide").get(optionHash["currSlide"] + 1);
-		
-		$(currentSlide).find("video").get(0).play();
-		
+
 		cyclePaused = false;
+
+		try {
+			$(currentSlide).find("video").get(0).play();
+		} catch(err) {}
+
 	});
-	
+
 	$slideshow.on("cycle-update-view", function(event, optionHash, slideOptionsHash, currentSlideEl) {
-		
-		if (isSmall()) return; 
-		
+
+		$('.cases__slide').css('z-index', 99);
+		$(currentSlideEl).css('z-index', 100)
+
+		if (isSmall()) return;
+
 		checkSlideshowStatus($slideshow);
-		
+
 		if (!cyclePaused) {
-			
-			$(".cycle-slideshow video").each(function(index, el){			
-			    el.pause();
-		    });	
-		    
-		    $(currentSlideEl).find("video").get(0).play();
+
+			$(".cycle-slideshow video").each(function(index, el){
+		    el.pause();
+	    });
+
+			try {
+				$(currentSlideEl).find("video").get(0).play();
+			} catch(err) {}
+
 		}
-	});	
-	
-	
+	});
+
+
 	// Mobile menu toggle
-	
+
 	$("#small-menu-toggle").on("click", function (e) {
-		
+
 		e.preventDefault();
-		
-		var el = $(this);		
+
+		var el = $(this);
 		var activeClass = "is-active";
 		var menu = $("#small-menu");
-		
-		if (el.hasClass(activeClass)) 
+
+		if (el.hasClass(activeClass))
 		{
 			el.removeClass(activeClass);
 			menu.slideUp();
 		}
-		else  
+		else
 		{
 			el.addClass(activeClass);
 			menu.slideDown();
 		}
 	});
-	
-	
+
+
 	// Accordion menus on footer
-	
+
 	$(".accordion-menu-toggle").on("click", function (e) {
-		
+
 		e.preventDefault();
 		e.stopPropagation();
-		
-		if (isSmall()) 
-		{				
+
+		if (isSmall())
+		{
 			var el = $(this);
 			var menu = el.next();
 			var activeClass = "is-active";
-			
-			if (el.hasClass(activeClass)) 
+
+			if (el.hasClass(activeClass))
 			{
 				menu.slideUp("fast");
 				el.removeClass(activeClass);
 			}
-			else 
+			else
 			{
 				el.addClass(activeClass);
 				menu.slideDown("fast");
 			}
 		}
 	});
-	
+
 	$(window).resize(function(){
-		toggleAccordionNavs();	
-	});	
-	
-	
-	// Scroll down to features section 
-	
+		toggleAccordionNavs();
+	});
+
+
+	// Scroll down to features section
+
 	$("#scroll-to-features").on("click", function (e) {
-		
+
 		e.preventDefault();
 		e.stopPropagation();
-		
-		$(window).scrollTo($("section.features"), 500);	
-	});	
-	
-	
+
+		$(window).scrollTo($("section.features"), 500);
+	});
+
+
 	// Make nav fixed on scroll down in docs pages
-	
+
 	var el = $(".docs .main-nav");
 	var elementPosition = el.offset();
-	
+
 	$(window).scroll(function(){
-		
+
 		if (!isSmall() && elementPosition) {
-		
+
 			if(($(window).scrollTop() + 32) > elementPosition.top){
 				el.css("position","fixed").css("top","24px");
 			} else {
 				el.css("position","static");
 			}
-		}   	
+		}
 	});
+
 });
